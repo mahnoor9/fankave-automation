93a69f38947720e779c277cdc982dd42edbcd75b
Saad Khan <muhammad.saad@emumba.com>
2022-02-04 18:30:23 +0500

    autodeskUpload

--- src/test/test.ts
+++ src/test/test.ts
@@ -2,6 +2,8 @@ import { test, expect, Page, chromium, firefox } from '@playwright/test';
 import { autodeskVideotask } from '../main/userSide/content.creation/tasks/autodeskVideo.tasks';
 import { recordAudio } from '../main/userSide/everlaw.testimonail/tasks/recordAudio.tasks';
 import { recordVideo } from '../main/userSide/everlaw.testimonail/tasks/recordVideo.task';
+import { autodeskPhototask } from '../main/userSide/content.creation/tasks/autodeskPhoto.tasks'
+import { autodeskUploadtask } from '../main/userSide/content.creation/tasks/autodeskUpload.tasks'
 
   // test('should add a new audio from the user side', async ({ page }) => {
   // await page.goto('https://dev.ccapps.fankave.com/everlaw/testimonials/');
@@ -15,8 +17,20 @@ import { recordVideo } from '../main/userSide/everlaw.testimonail/tasks/recordVi
   //    console.log(recording)
   //   });
 
-  test('Should create a video', async ({ page }) => {
-      await page.goto('https://dev.ccapps.fankave.com/contentcreation/');
-       let autoVideo = await autodeskVideotask(page);
-       console.log(autoVideo)
-      });
+  // test('Should create a video', async ({ page }) => {
+  //     await page.goto('https://dev.ccapps.fankave.com/contentcreation/');
+  //      let autoVideo = await autodeskVideotask(page);
+  //      console.log(autoVideo)
+  //     });
+
+  // test('Should capture a photo', async ({ page }) => {
+  //   await page.goto('https://dev.ccapps.fankave.com/contentcreation/');
+  //    let autoPhoto = await autodeskPhototask(page);
+  //    console.log(autoPhoto)
+  //   });
+
+  test('Should Upload a photo', async ({ page }) => {
+    await page.goto('https://dev.ccapps.fankave.com/contentcreation/');
+     let autoImage = await autodeskUploadtask(page);
+     console.log(autoImage)
+    });

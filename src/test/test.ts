8601953b5eca97c0c4f44e4f761acebbe59d6fb4
Saad Khan <muhammad.saad@emumba.com>
2022-02-04 14:26:44 +0500

    commit

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,6 +1,6 @@
 import { test, expect, Page, chromium, firefox } from '@playwright/test';
-import { recordAudio } from '../main/userSide/tasks/recordAudio.tasks';
-import { recordVideo } from '../main/userSide/tasks/recordVideo.task';
+import { recordAudio } from '../main/userSide/everlaw.testimonail/tasks/recordAudio.tasks';
+import { recordVideo } from '../main/userSide/everlaw.testimonail/tasks/recordVideo.task';
 
   test('should add a new audio from the user side', async ({ page }) => {
   await page.goto('https://dev.ccapps.fankave.com/everlaw/testimonials/');

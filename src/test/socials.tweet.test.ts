961d59f01702ebdd8f6ae6c48006ec17779c1ec1
Saad Khan <muhammad.saad@emumba.com>
2022-02-10 18:23:13 +0500

    Check Video

--- src/test/socials.tweet.test.ts
+++ src/test/socials.tweet.test.ts
@@ -8,20 +8,20 @@ test.beforeEach(async ({ page }) => {
     await page.goto('https://dev.ccapps.fankave.com/contentcreation/');
   });
 
-test('Should Capture a photo and tweet it', async ({ page }) => {
+test('Should Capture a Photo and tweet it', async ({ page }) => {
   await autodeskPhototask(page);
   let autotweet = await tweetAutodesk(page);
   expect(autotweet.toLocaleLowerCase()).toContain("posted on twitter")
 });
 
-test('Should Upload a photo and tweet it', async ({ page }) => {
+test('Should Upload a Photo and tweet it', async ({ page }) => {
  await autodeskUploadtask(page);
  let autotweet = await tweetAutodesk(page);
  expect(autotweet.toLocaleLowerCase()).toContain("posted on twitter")
 });
 
-test('Should capture a Video and tweet it', async ({ page }) => {
- await autodeskVideotask(page);
- let autotweet = await tweetAutodesk(page);
- await expect(autotweet.toLocaleLowerCase()).toContain("posted on twitter")
-});
+// test('Should capture a Video and tweet it', async ({ page }) => {
+//  await autodeskVideotask(page);
+//  let autotweet = await tweetAutodesk(page);
+//  expect(autotweet.toLocaleLowerCase()).toContain("posted on twitter")
+// });

c9136db740eeccd45a30d0973548429215251097
Saad Khan <muhammad.saad@emumba.com>
2022-03-02 15:48:48 +0500

    Transcribed Audio Flow Added and Functional

--- src/test/Demo Experience/Admin Side/demo.adminTranscribe.test.ts
+++ src/test/Demo Experience/Admin Side/demo.adminTranscribe.test.ts
@@ -3,9 +3,11 @@ import { getTexttoCheck } from '../../../main/adminSide/tasks/adminTranscribe.ta
 import { googleSignIn } from "../../../main/adminSide/tasks/adminLogin.tasks";
 import { adminSide } from '../../JSONtestdata/demo.json'
 import { searchFankave } from '../../../main/adminSide/tasks/adminSearchFankave.actions';
+import { sendMedia } from '../../PublishAPI';
 
 test('should check the latest media transcribing for the media sent by a user from the admin side', async ({ browser }) => {
     
+    await sendMedia();
     let page = await browser.newPage();
     await page.goto(adminSide.URL);
     await googleSignIn(page, adminSide.googleAccount, adminSide.googlePassword);
@@ -13,6 +15,7 @@ test('should check the latest media transcribing for the media sent by a user fr
     await page.waitForTimeout(1000);
     let texttoCheck = await getTexttoCheck(page);
     console.log(texttoCheck);
-    expect(texttoCheck).toContain("Testing testing one, two, three. Testing testing.")
+    expect(texttoCheck).toContain("Hello, this is a test for transcribed text and this is the first testimonial for record audio feature.")
     
 });
+
